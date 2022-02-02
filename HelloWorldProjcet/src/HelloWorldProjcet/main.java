936acc82021ea335f98a91c1925dd4b961d889e5
acer <acer@LAPTOP-1FALFG3N>
2022-02-02 18:45:27 +0700

    lab1

---
+++ HelloWorldProjcet/src/HelloWorldProjcet/main.java
@@ -0,0 +1,14 @@
+package HelloWorldProjcet;
+
+public class main {
+	
+	public main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println( "Hello World" ); // TODO Auto-generated method stub
+
+	}
+
+}
